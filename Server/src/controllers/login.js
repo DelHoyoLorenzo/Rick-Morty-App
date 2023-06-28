e797d32cf0b585afd123dc0a2daaaf8352f1cb47
DelHoyoLorenzo <delhoyo.lorenzo@gmail.com>
2023-06-27 23:41:07 -0300

    update

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,19 +1,21 @@
-const users = require('../utils/user');
+const users = require("../utils/user");
 
-function loginHandler(req,res){
-    const {email, password} = req.query;
+function loginHandler(req, res) {
+  try {
+    const { email, password } = req.query;
 
-    let user = users.find((user)=>{
-        if(user.email === email && user.password === password){
-            return user;
-        }
-    })
+    let user = users.find((user) => {
+      if (user.email === email && user.password === password) {
+        return user;
+      }
+    });
 
-    if(user){
-        res.status(200).json({ access: true })
-    }else{
-        res.status(400).json({ access: false })
+    if (user) {
+      res.status(200).json({ access: true });
+    } else {
+      res.status(400).json({ access: false });
     }
+  } catch (error) {}
 }
 
-module.exports = {loginHandler}; 
+module.exports = { loginHandler };
